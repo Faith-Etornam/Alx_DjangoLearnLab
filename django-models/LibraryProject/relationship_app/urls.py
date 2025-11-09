08a0f7a4d92764db93df043382745e2795fbeaae
Faith Etornam <faithgbadegbe1@gmail.com>
2025-11-09 22:43:24 +0000

    Extending the User authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,9 +12,10 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
+    # Book management URLs with permissions
     path('books/', views.book_list, name='book_list'),
-    path('add_book/', views.add_book, name='add_book'),
+    path('books/add/', views.add_book, name='add_book'),
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
-    path('edit_books/', views.edit_book, name='edit_book'),
-    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
+    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),  # Add this line
+    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book')
 ]
