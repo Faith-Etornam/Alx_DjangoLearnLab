1a2a115929a7275a6fffa8f8d09dd87b9689e6fb
Faith Etornam <faithgbadegbe1@gmail.com>
2025-11-09 22:41:49 +0000

    Extending the User authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
     path('books/', views.book_list, name='book_list'),
-    path('books/add/', views.add_book, name='add_book'),
+    path('add_book/', views.add_book, name='add_book'),
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
-    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('edit_books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
