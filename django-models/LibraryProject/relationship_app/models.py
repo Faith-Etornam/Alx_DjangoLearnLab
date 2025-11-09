b7e20f4195889da954317a146938cc0d1de28f8a
Faith Etornam <faithgbadegbe1@gmail.com>
2025-11-09 12:15:07 +0000

    Working with the ORM in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,6 +8,9 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class Library(models.Model):
     name = models.CharField(max_length=255)
     books = models.ManyToManyField(Book)
