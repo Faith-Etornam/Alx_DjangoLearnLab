fb96b8953a208f93fb43a3eb9624faac1172e2c8
Faith Etornam <faithgbadegbe1@gmail.com>
2025-11-09 12:17:23 +0000

    Working with the ORM in Django

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,24 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# 1. Query all books by a specific author (ForeignKey)
+print("All books by J.K. Rowling:")
+author = Author.objects.get(name="J.K. Rowling")
+books = Book.objects.filter(author=author)
+for book in books:
+    print(f"- {book.title}")
+
+print()
+
+# 2. List all books in a library (ManyToMany)
+print("All books in Central Public Library:")
+library = Library.objects.get(name="Central Public Library")
+for book in library.books.all():
+    print(f"- {book.title}")
+
+print()
+
+# 3. Retrieve the librarian for a library (OneToOne)
+print("Librarian for Central Public Library:")
+library = Library.objects.get(name="Central Public Library")
+librarian = Librarian.objects.get(library=library)
+print(f"- {librarian.name}")
