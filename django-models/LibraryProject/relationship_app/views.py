1ef7875a045bc15faf2a6dd48cc7f32d8eb9b2f4
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-11 12:04:00 +0000

    Created a function based view for my books_list endpoint

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
+from .models import Book, Author
 from django.shortcuts import render
 
+
 # Create your views here.
+def book_list(request):
+    queryset = Book.objects.all()
+    Author.objects.create(name="Stan Lee")
+    Author.objects.create(name="Stan Pee")
+    Book.objects.create(title='Spider-Man', author_id=1)
+    Book.objects.create(title='Iron-Man', author_id=1)
+    Book.objects.create(title='Captain America', author_id=1)
+    context = {'books': queryset}
+    return render(request, 'list_books.html', context)
