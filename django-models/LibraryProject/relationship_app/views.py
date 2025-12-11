f49828a8ff67566cdb63b9850539bc55d0049de8
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-11 13:08:32 +0000

    Building the class based view for book detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from .models import Book, Author
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book
+from .models import Library
 
 
 # Create your views here.
@@ -8,3 +10,8 @@ def book_list(request):
     queryset = Book.objects.all()
     context = {'books': queryset}
     return render(request, 'relationship_app/list_books.html', context)
+
+class BookDetail(DetailView):
+    def get_queryset(self):
+        return Book.objects.filter(id=self.kwargs['id'])
+
