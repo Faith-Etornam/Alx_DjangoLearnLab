1097d28d408a48376ba7c1e1ef50c9bf3316b5ec
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-13 23:24:31 +0000

    Implemented a book form and ExampleForm classes

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,2 +1,11 @@
-from django.forms import forms
+from django import forms
+from .models import Book
 
+class BookForm(forms.ModelForm):
+    class Meta: 
+        model = Book
+        fields = ['title', 'author']
+
+class ExampleForm(forms.Form):
+    name= forms.CharField(max_length=255)
+    email = forms.EmailField()
