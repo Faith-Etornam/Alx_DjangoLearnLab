d844a5f9c8b31c60e604d69b56f8b056085d0ca3
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-15 16:16:22 +0000

    Added urls for all the views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ListView, CreateView
+from .views import ListView, CreateView, UpdateView, DeleteView, DetailView
 
 urlpatterns = [
     path('books/', ListView.as_view(), name='list-book'),
-    path('books/create', CreateView.as_view(), name='create-book')
+    path('books/detail', DetailView.as_view(), name='detail-book'),
+    path('books/create', CreateView.as_view(), name='create-book'),
+    path('books/update', UpdateView.as_view(), name='update-book'),
+    path('books/delete', DeleteView.as_view(), name='delete-book')
 ]
