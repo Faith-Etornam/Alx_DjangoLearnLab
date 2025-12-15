b9ed104dcb28c81c5b84494c994a9afc6b996704
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-15 15:14:28 +0000

    Added the AuthorSerializer and nested the BookSerializer with it

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,6 +1,6 @@
 from django.utils.timezone import now
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Author
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +10,13 @@ class BookSerializer(serializers.ModelSerializer):
     def validate_publication_year(self, value):
         if value > now().year:
             raise serializers.ValidationError('Year cannot be greater than the current year')
+        
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer()
+    class Meta:
+        model = Author
+        fields = '__all__'
+
         
 
         
