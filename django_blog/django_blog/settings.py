288693f59433cf67c88f032055e724e990cfc2ab
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-16 13:41:23 +0000

    Added templates and static files and configured the settings for locating these resources

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -52,10 +52,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'django_blog.urls'
 
+STATIC_DIRS = [
+    BASE_DIR / 'static',
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
