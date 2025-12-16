f5dbbbb6efad8960e6938b7c24227ee5d1f219fc
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-16 12:18:36 +0000

    Changed database to mysql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,11 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'NAME': 'blog',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
