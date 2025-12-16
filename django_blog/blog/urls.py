7d84a9b1ddea21eb17099b6be7828bb78f6701b2
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-16 15:30:07 +0000

    Refactored my register handler

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('register/', views.register, name='register'),
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='blog/logout.html')),
     path('posts/', views.home, name='posts')
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,21 +1,17 @@
 from django.contrib.auth import login
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from .forms import RegisterForm
 
 # Create your views here.
-def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-    else:
-        form = RegisterForm()
-    return render(request, 'blog/registration.html', {'form': form})
-
 def home(request):
-    # This assumes you will create a file named 'home.html' 
-    # inside blog/templates/blog/
     return render(request, 'base.html')
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'blog/registration.html'
+    success_url = reverse_lazy('home')
+
+
+
