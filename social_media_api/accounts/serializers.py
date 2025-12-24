930f6f8c1d663e5cf0b0fe109b28342ce7eae1e4
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-24 23:23:14 +0000

    Added the UserRegistration serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,9 +1,13 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 class UserRegistration(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password', 'bio', 'profile_picture']
+
+
