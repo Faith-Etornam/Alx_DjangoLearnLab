3bcb28dabd731c00ada27b0d4ce9b1af7831909a
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-25 15:15:50 +0000

    Added viewsets for posts and comments

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from posts.models import Comment, Post
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView
 
 # Create your views here.
+
+class Posts(ModelViewSet):
+    queryset = Post.objects.all()
+    # serializer_class = 
+
+class Comments(ModelViewSet):
+    queryset = Comment.objects.all()
+
 class Register(CreateAPIView):
     pass
 
