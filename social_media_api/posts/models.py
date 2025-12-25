9303d0e3bc0e751e9de62f58fd901a2eca98574a
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-25 13:33:35 +0000

    Created the Post model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True
+)
+    
