fb7755ce0f086165ee2866f1d98562ba0cfb645d
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-25 13:16:37 +0000

    Assigned token to a user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,4 +10,16 @@ class UserRegistration(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password', 'bio', 'profile_picture']
 
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+
+        Token.objects.create(user=user)
+        return user
+
 
