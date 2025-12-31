8f3ad820e047d56efb1cfcc908bcf59debdf49b3
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-31 16:28:51 +0000

    Implemented a custom permission named IsOwnerOrReadOnly

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from posts.models import Comment, Post
 from rest_framework import viewsets
+from rest_framework import permissions
+from posts.models import Comment, Post
+from .serializers import PostSerializer
 
 # Create your views here.
 
