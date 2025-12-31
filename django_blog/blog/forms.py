9dcb530dcfcea42fc66d25071a00e3fc983c0748
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-31 21:39:02 +0000

    Added the necessay urls for comments

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Comment
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -29,3 +30,14 @@ class CommentForm(forms.ModelForm):
         if not content:
             raise forms.ValidationError('Comment cannot be empty!')
         return content
+    
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'rows': 5, 'class': 'form-control'}),
+            'tags': TagWidget(),
+        }
