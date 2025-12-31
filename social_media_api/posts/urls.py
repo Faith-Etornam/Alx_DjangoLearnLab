1dcc9e24c61d2a7613db94f19888102a7e607825
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-31 17:21:44 +0000

    Configured the url for unliking a post

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserFeedView, LikePostView, 
+from .views import UserFeedView, LikePostView, UnlikePostView
 
 urlpatterns = [
     path('feed/', UserFeedView.as_view(), name='user-feed'),
     path("post/<int:pk>/like/", LikePostView.as_view(), name="like-post"),
+    path("post/<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
 ]
