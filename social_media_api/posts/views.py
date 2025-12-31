d428171e350cdc710f4e45b38900a44da7377c36
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-31 17:12:09 +0000

    Implemented the LikePostView

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,14 @@
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import generics
-from posts.models import Comment, Post
+from rest_framework import status
+from posts.models import Comment, Post, Like
 from .serializers import PostSerializer
 from .permissions import IsOwnerOrReadOnly
 from .paginations import PostPagination
+from notifications.models import Notification
 
 # Create your views here.
 
@@ -33,4 +36,32 @@ class UserFeedView(generics.GenericAPIView):
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+    
+
+
+
+class LikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self, request, pk, *args, **kwargs):
+        post = generics.get_object_or_404(Post, pk=pk)
+        
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+        
+        if not created:
+            return Response({
+                "detail": "You've already liked this post."
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        if post.author != request.user:
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked your post",
+                content_type=ContentType.objects.get_for_model(Post),
+                object_id=post.id
+            )
+
+        return Response({"detail": "Post liked."}, status=status.HTTP_201_CREATED)
+
 
