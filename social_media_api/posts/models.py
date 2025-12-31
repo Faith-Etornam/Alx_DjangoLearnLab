9e6fab88a9a864ba8b5bc2b8c18755a9683c4126
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-31 16:48:54 +0000

    Added the Like Model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -16,3 +16,15 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    
+    def __str__(self):
+        return f"Like by {self.user.username} on {self.post.title}"
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'post'], name='unique_like')
+        ]
