8072f81a683bcd59357c8179a1a786de2eb3b01d
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-31 16:40:01 +0000

    Added the UserFeedView

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets, filters
+from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework import generics
 from posts.models import Comment, Post
 from .serializers import PostSerializer
 from .permissions import IsOwnerOrReadOnly
@@ -19,4 +21,16 @@ class PostViewset(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 class Comments(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
+
+class UserFeedView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        
+        following_users = request.user.following.all()
+
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
 
