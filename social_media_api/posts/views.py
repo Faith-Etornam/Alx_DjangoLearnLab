599941d23e584da44febe15dcd02eb8af062bd92
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-31 16:26:30 +0000

    Implemented the PostSerializer

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -4,10 +4,16 @@ from rest_framework import viewsets
 
 # Create your views here.
 
-class Posts(viewsets.ModelViewSet):
+class PostViewset(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    # serializer_class = 
-
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    pagination_class = PostPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+    
+    def create(self, serializer):
+        serializer.save(author=self.request.user)
 class Comments(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
 
