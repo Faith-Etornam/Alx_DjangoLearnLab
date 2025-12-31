f99d4a110e9d4936122cb83554b1869a708ccbfd
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-31 16:43:37 +0000

    Configured the urls as well

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView, LogoutView, FollowUserView
+from .views import RegisterView, LoginView, ProfileView, LogoutView, FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='registration'),
